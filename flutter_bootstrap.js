d01ee818427d88dacd5cacd7ddba0e81c9a289b2
Terlinet <terlinetdarth@gmail.com>
2026-05-05 14:27:04 +0000

    Deploying to gh-pages from @ Terlinet/terlinetscreen@8079a2bd5d8727df900f4e03398e10742fde6e8b 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2613849596" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2991047906" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
